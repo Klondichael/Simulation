32ae7d2275b74e94ecc6a25078c978a9b9af9004
Klondichael <michael.j.hickey@outlook.com>
2026-05-16 19:24:55 -0400

    Refactor Guy class and improve movement logic

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import math
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
-FPS = 60
 running = True
 font = pygame.freetype.SysFont("Arial", 24)
 
@@ -17,41 +16,48 @@ with open("adjectives.txt", "r") as file:
 with open("names.txt", "r") as file:
     names = file.read().splitlines()
 
+FPS = 60
+show_names = True
+num_guys = 50
+turn_radius = 10 * ((2*math.pi) / 360)
+
 class Guy:
 
     def __init__(self):
         self.name = f"{random.choice(adjectives)} {random.choice(names)}"
         self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        self.speed = random.randint(1, 10) * 10
-        self.position = (random.randint(0, screen.get_width()), random.randint(0, screen.get_height()))
-        self.size = random.randint(5, 50)
-        self.direction = (math.cos(random.uniform(0, 2*math.pi)), math.sin(random.uniform(0, 2*math.pi)))
+        self.speed = random.randint(5, 10) * 10
+        self.size = random.randint(10, 50)
+        self.position = (random.randint(self.size, screen.get_width()-self.size), random.randint(self.size, screen.get_height()-self.size))
+        self.direction = (random.uniform(0, 2*math.pi))
         self.turn_weight = random.uniform(0.005, 0.01)
     
     def draw_guy(self):
         pygame.draw.circle(screen, self.color, self.position, self.size)
+
+    def draw_name(self):
         text_rect = font.get_rect(self.name)
         text_rect.center = (self.position[0], self.position[1] - (self.size+30))
         font.render_to(screen, text_rect, self.name, "white")
 
     def move_guy(self, dt):
-        next_pos = (self.position[0] + (self.speed*self.direction[0]*dt), self.position[1] + (self.speed*self.direction[1]*dt))
-        if not ((next_pos[0] < 0 or next_pos[0] > screen.get_width()) or (next_pos[1] < 0 or next_pos[1] > screen.get_height())):
+        next_pos = (self.position[0] + (self.speed*math.cos(self.direction)*dt), self.position[1] + (self.speed*math.sin(self.direction)*dt))
+        if not ((next_pos[0]-self.size < 0 or next_pos[0]+self.size > screen.get_width()) or (next_pos[1]-self.size < 0 or next_pos[1]+self.size > screen.get_height())):
             self.position = next_pos
-        elif (next_pos[0] < 0 or next_pos[0] > screen.get_width()):
-            self.direction = (self.direction[0] * -1, self.direction[1])
-            self.position = (self.position[0] + (self.speed*self.direction[0]*dt), self.position[1] + (self.speed*self.direction[1]*dt))
+        elif (next_pos[0]-self.size < 0 or next_pos[0]+self.size > screen.get_width()):
+            self.direction = (math.pi/2) + ((math.pi/2)-self.direction)
+            self.position = (self.position[0] + (self.speed*math.cos(self.direction)*dt), self.position[1] + (self.speed*math.sin(self.direction)*dt))
         else:
-            self.direction = (self.direction[0], self.direction[1] * -1)
-            self.position = (self.position[0] + (self.speed*self.direction[0]*dt), self.position[1] + (self.speed*self.direction[1]*dt))
+            self.direction = math.pi + (math.pi-self.direction)
+            self.position = (self.position[0] + (self.speed*math.cos(self.direction)*dt), self.position[1] + (self.speed*math.sin(self.direction)*dt))
 
     def turn_guy(self):
         if random.random() < self.turn_weight:
-            self.direction = (math.cos(random.uniform(0, 2*math.pi)), math.sin(random.uniform(0, 2*math.pi)))
+            self.direction = (random.uniform(self.direction-turn_radius, self.direction+turn_radius))
 
 guys = []
 
-for i in range(1):
+for i in range(num_guys):
     guys.append(Guy())
 
 while running:
@@ -65,8 +71,9 @@ while running:
 
     for guy in guys:
         guy.draw_guy()
+        if show_names: guy.draw_name()
         guy.move_guy(dt)
-        #guy.turn_guy()
+        guy.turn_guy()
 
     pygame.display.flip()
 
